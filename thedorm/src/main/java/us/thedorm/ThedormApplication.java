e7d12f8a8e7af1a2a9b1f2f8913ba71812a513d2
hopdinhp02 <hopdinhp02@gmail.com>
2023-02-16 17:53:16 +0700

    modify attribute for booking bed

--- thedorm/src/main/java/us/thedorm/ThedormApplication.java
+++ thedorm/src/main/java/us/thedorm/ThedormApplication.java
@@ -1,9 +1,8 @@
 package us.thedorm;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import us.thedorm.repositories.BranchRepository;
 
 @SpringBootApplication
 public class ThedormApplication {
