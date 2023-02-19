33caf99935fc0ad80e4edfd1ff31089f1c3b427d
hopdinhp02 <hopdinhp02@gmail.com>
2023-02-19 12:13:57 +0700

    delete role class

--- thedorm/src/main/java/us/thedorm/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package us.thedorm.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import us.thedorm.models.Role;
-
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
