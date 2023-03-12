a0e4a401fb4d966be701b786a20db2cbdbf2872d
DucAnh <nguyenhuuducanh02@gmail.com>
2023-03-12 11:19:59 +0700

    Add electric water usage in front end

---
+++ thedorm/src/main/resources/public/js/electricWaterUsage.js
@@ -0,0 +1,50 @@
+electricWaterUsage();
+
+function electricWaterUsage() {
+    let url = "http://localhost:8081/api/v1/elec-water-usages/all-electric-water-usage-of-resident";
+    let electricWaterUsageTable = document.getElementById("electricWaterUsage");
+    let electricWaterUsage = ``;
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem("jwt")}`
+        }
+    })
+
+        .then(response => response.json())
+
+        .then(dataJson => {
+            console.log(dataJson);
+            // <th>ID</th>
+            // <th>roomID</th>
+            // <th>electricUsage</th>
+            // <th>waterUsage</th>
+            // <th>createdDate</th>
+            // <th>electricEnd</th>
+            // <th>electricStart</th>
+            // <th>monthPay</th>
+            // <th>waterStart</th>
+            // <th>waterEnd</th>
+            dataJson.data.forEach(element => {
+                electricWaterUsage += `<tr>
+                <td>${element.id}</td>
+                <td>${element.room.id}</td>
+                <td>${element.electricUsage}</td>
+                <td>${element.waterUsage}</td>
+                <td>${element.createdDate}</td>
+                <td>${element.electricEnd}</td>
+                <td>${element.electricStart}</td>
+                <td>${element.monthPay}</td>
+                <td>${element.waterStart}</td>
+                <td>${element.waterEnd}</td>
+                </tr>`;
+            });
+
+            electricWaterUsageTable.innerHTML = electricWaterUsage;
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
