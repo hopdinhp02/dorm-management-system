245ad714d0282c261a7076739e178b399cf0ba53
quocanh07 <anhquoc71202@gmail.com>
2023-03-17 10:14:07 +0700

    commit tast 15/3

--- thedorm/src/main/java/us/thedorm/controllers/ResidentHistoryController.java
+++ thedorm/src/main/java/us/thedorm/controllers/ResidentHistoryController.java
@@ -294,6 +294,8 @@ public class ResidentHistoryController {
     }
 
 
+
+
 }
 
 
--- thedorm/src/main/java/us/thedorm/repositories/BookingScheduleRepository.java
+++ thedorm/src/main/java/us/thedorm/repositories/BookingScheduleRepository.java
@@ -1,12 +1,18 @@
 package us.thedorm.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import us.thedorm.models.BookingSchedule;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookingScheduleRepository extends JpaRepository<BookingSchedule,Long> {
 
-    Optional<BookingSchedule> findBookingScheduleByBranch_Id(long id);
     Optional<BookingSchedule> findTopByBranch_IdOrderByIdDesc(long id);
+
+    @Query(value =
+            "   select top 2 *From [booking_schedule] where [booking_schedule].branch_id =:branch_id  order by id desc", nativeQuery = true)
+    List<BookingSchedule> findBookingScheduleLastSemesterByBranch(@Param("branch_id") long id);
 }
